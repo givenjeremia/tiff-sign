20b7eafccc5eaabb1293e1bae07ff173fff0deeb
givenjeremia <jeremiagiven@gmail.com>
2022-03-16 11:23:44 +0700

    first

---
+++ index.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/TiffSign.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Bowlby One SC' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/037ed367a5.js" crossorigin="anonymous"></script>
+    <title>Tiff-Sign</title>
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light p-3 border-bottom border-success border-4">
+        <div class="container">
+            <a class="navbar-brand italic" href="#">
+                <img src="img/TiffSign.png" alt="" height="30" class="d-inline-block align-text-top me-3">
+                <em>Tiff-Sign</em>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#home"><strong>HOME</strong></a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="#products"><strong>PRODUCTS</strong></a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="#service"><strong>SERVICE</strong></a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="#contact"><strong>CONTACT</strong></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-none d-lg-flex">
+                <a href="https://api.whatsapp.com/send?phone=62083856304159&text=Hi,%20Tiff-Sign!"
+                    class="rounded-circle bg-success text-center sosmed me-3"><i
+                        class="fab fa-whatsapp position-relative" style="top: calc(50% - 12px);"></i></a>
+                <a href="https://www.instagram.com/tiff_sign/"
+                    class="rounded-circle bg-success text-center sosmed me-3"><i
+                        class="fab fa-instagram position-relative" style="top: calc(50% - 12px);"></i></a>
+                <a href="#" class="rounded-circle bg-success text-center sosmed"><i
+                        class="fab fa-facebook position-relative" style="top: calc(50% - 12px);"></i></a>
+            </div>
+        </div>
+    </nav>
+
+    <section class="row m-0 pt-5" id='home'>
+        <div class="col-lg-5 bg-secondary d-flex flex-column justify-content-evenly">
+            <div class="px-5">
+                <img src="img/vector/welcome.png" class="img-fluid" alt="...">
+            </div>
+            <div>
+                <h1 class="display-4 text-center">Make your design into reality now!</h1>
+            </div>
+        </div>
+        <div class="col-lg-7 p-0">
+            <img src="img/billboardlogo.png" class="img-fluid">
+        </div>
+    </section>
+
+    <section class="bg-light" id="about">
+        <div class="container py-4">
+            <div class="row align-items-center">
+                <div class="col-lg-5">
+                    <img src="img/vector/description.png" class="img-fluid" alt="...">
+                </div>
+                <div class="col-lg-7">
+                    <figure class="m-5">
+                        <blockquote class="blockquote">
+                            <p class="pb-2"><em>Seiring dengan berkembangnya teknologi komputer, digitalisasi dan
+                                    permesinan, maka kami yang bergerak di bidang produksi signage ikut ambil bagian
+                                    dalam peluang dan mengisi
+                                    kebutuhan akan hal ini .Kami spesialis dalam memproduksi segala kebutuhan signage
+                                    dan kaitannya dengan
+                                    promosi, seperti neon box, sticker cutting dan printing, petunjuk arah gedung, nama
+                                    ruang baik di gedung
+                                    maupun office, nama toko - letter huruf timbul, akrilik display dan lain sebagainya.
+                                    Kami sangat perhatian dengan hasil yang baik dan keakurasian yang tinggi dalam hal
+                                    pembuatan signage - mewujudkan impian para klien dan disainer signage.</em></p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer text-end">
+                            Tiff-sign Team
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="products" class="bg-light py-5 ">
+        <div class="container py-5">
+            <p class="display-4 text-center">OUR PRODUCT</p>
+            <div class="row justify-content-around">
+                <div class="col-lg-5 my-5">
+                    <div class="card px-5 pt-5 pb-4 rounded-3 shadow">
+                        <img src="img/billboard.png" class="card-img-top rounded-bottom  " alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">BILLBOARD</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                            <a href="https://www.instagram.com/tiff_sign/" class="btn btn-primary">Lihat Selengkapnya
+                                -></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 my-5">
+                    <div class="card px-5 pt-5 pb-4 rounded-3 shadow">
+                        <img src="img/Effect.png" class="card-img-top rounded-bottom  " alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">NEON BOX</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                            <a href="https://www.instagram.com/tiff_sign/" class="btn btn-primary">Lihat Selengkapnya
+                                -></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 my-5">
+                    <div class="card px-5 pt-5 pb-4 rounded-3 shadow">
+                        <img src="img/logomoc.jpg" class="card-img-top rounded-bottom  " alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">LETTER TIMBUL</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                            <a href="https://www.instagram.com/tiff_sign/" class="btn btn-primary">Lihat Selengkapnya
+                                -></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 my-5">
+                    <div class="card px-5 pt-5 pb-4 rounded-3 shadow">
+                        <img src="img/sticker.png" class="card-img-top rounded-bottom  " alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">PROMOTIONAL STICKER</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the card's content.</p>
+                            <a href="https://www.instagram.com/tiff_sign/" class="btn btn-primary">Lihat Selengkapnya
+                                -></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="service" class=" py-5">
+        <div class="container">
+            <h1 class="display-5 text-center mb-5">OUR SERVICE</h1>
+            <div class="row justify-content-around align-items-center" style="font-family: 'Open-Sans'">
+                <div class="col-lg-4">
+                    <div><img src="img/vector/signageproduction.png" class="img-fluid" alt=""></div>
+                </div>
+                <div class="col-lg-4">
+                    <h4 class="display-1 text-shadow text-center pt-5 pt-lg-0" style="font-size: 5rem !important;">
+                        Signage
+                        Production</h4>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ps-5 ms-5 ps-lg-0 ms-lg-0"><img src="img/vector/service.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="contact" class="bg-primary">
+        <div class="container p-3">
+            <div class="row justify-content-center">
+                <div class="col-lg-3 d-lg-flex align-items-center justify-content-end" id="contact-tiff-sign">
+                    <img src="img/TiffSign.png" class="bg-primary border-success" height="100">
+                </div>
+                <div class="col-lg-4">
+                    <h1 class="h1 text-center text-lg-start">Contact Us on:</h1>
+                    <p class="h6 text-center text-lg-start"><i class="fab fa-whatsapp me-1"></i>08923552164</p>
+                    <p class="h6 text-center text-lg-start"><i
+                            class="far fa-envelope me-1"></i>tiffanyemmanuela@gmail.com</p>
+                    <p class="h6 text-center text-lg-start"><i class="fab fa-instagram me-1"></i>tiff_sign</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <a href="https://api.whatsapp.com/send?phone=62083856304159&text=Hi,%20Tiff-Sign!" tabindex="0"
+        data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Contact Us!" data-bs-placement="left"
+        class="float d-lg-none" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+        $('.navbar-nav a').on('click', function () {
+            $('.navbar-nav').find('li .active').removeClass('active');
+            $(this).addClass('active');
+        });
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+        integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
+        integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous">
+    </script>
+    -->
+</body>
+
+</html>
